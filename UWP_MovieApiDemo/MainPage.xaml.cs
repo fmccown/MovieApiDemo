b7d776af3fde82a29b7cef1a200f215d3cf38b40
Frank McCown <fmccown@harding.edu>
2021-11-16 08:20:23 -0600

    Add solution

---
+++ UWP_MovieApiDemo/MainPage.xaml.cs
@@ -0,0 +1,76 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.ObjectModel;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace UWP_MovieApiDemo
+{
+    public sealed partial class MainPage : Page
+    {
+        const string ApiKey = "YOUR_API_KEY";
+
+        private ObservableCollection<Movie> movieList;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            movieList = new ObservableCollection<Movie>();
+        }
+
+        public async static Task<Movie> GetMovie(string imdbId)
+        {
+            Movie movie = null;
+
+            if (imdbId.StartsWith("tt"))
+            {
+                const string baseUrl = "http://www.omdbapi.com/";
+                Uri requestUrl = new Uri($"{baseUrl}?i={imdbId}&apiKey={ApiKey}");
+
+                using (var httpClient = new HttpClient())
+                {
+                    try
+                    {
+                        var json = await httpClient.GetStringAsync(requestUrl);
+                        movie = JsonConvert.DeserializeObject<Movie>(json);
+                    }
+                    catch (HttpRequestException ex)
+                    {
+                        var errorDialog = new ContentDialog()
+                        {
+                            Title = "HttpClient error",
+                            Content = ex.Message,
+                            CloseButtonText = "OK"
+                        };
+
+                        await errorDialog.ShowAsync();
+                    }
+                }
+            }
+            else
+            {
+                var errorDialog = new ContentDialog()
+                {
+                    Title = "Invalid ID",
+                    Content = "Please supply an IMDB ID, which must start with \"tt\".",
+                    CloseButtonText = "OK"
+                };
+
+                await errorDialog.ShowAsync();
+            }            
+
+            return movie;
+        }
+
+        private async void fetchButton_Click(object sender, RoutedEventArgs e)
+        {
+            Movie movie = await GetMovie(imdbIdTextBox.Text);
+            if (movie != null && movie.Title != "")
+            {
+                movieList.Add(movie);
+            }
+        }
+    }
+}
---
+++ UWP_MovieApiDemo/Movie.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace UWP_MovieApiDemo
+{
+    public class Movie
+    {
+        // Subset of fields from the API
+
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public string Rated { get; set; }
+        public string Released { get; set; }
+        public string Runtime { get; set; }
+        public string Genre { get; set; }
+        public string Director { get; set; }
+        public string Writer { get; set; }
+        public string Actors { get; set; }
+        public string Plot { get; set; }
+        public string Language { get; set; }
+        public string Country { get; set; }
+        public string Awards { get; set; }
+        public string Poster { get; set; }
+        public List<Rating> Ratings { get; set; }        
+        public string Metascore { get; set; }
+        public double imdbRating { get; set; }        
+        public string imdbVotes { get; set; }
+    }
+
+    public class Rating
+    {
+        public string Source { get; set; }
+        public string Value { get; set; }
+    }
+}
